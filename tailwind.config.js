344123bd887edc9f9e2d956b949356434dd6ac20
Rashmi Sahani <73373335+rash-22@users.noreply.github.com>
2023-08-31 13:00:56 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    fontFamily: {
+      mullish: ["Mulish", "sans-serif"],
+    },
+    colors: {
+      deepBlue: "#02042a",
+      lightBlue: "#2b84ea",
+      lightBlue300: "#4b94ed",
+      lightBlue500: "#0b72e7",
+      greenLight: "#61cea6",
+      grayText: "#818597",
+      lightGray: "#e2e2e2",
+      grayBlue: "#344a6c",
+      deepBlueHead: "#162f56",
+      gray2: "#525a76",
+      white: "#fff",
+      gray600: "rgb(75 85 99)",
+      gray400: " rgb(148 163 184)",
+      gray300: "rgb(203 213 225)",
+      green500: "rgb(34 197 94)"
+    },
+  },
+  plugins: [],
+};
